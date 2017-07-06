60f59c31bdc5b1036993f647ba7458c71e410f76
alexpadilla505 <alexandra309229@gmail.com>
2017-07-06 12:37:37 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -5,16 +5,15 @@ import math
 t = Turtle()
 
 # Set Up your screen and starting position.
-setup(500,300)
-x_pos = -250
-y_pos = -150
+setup(400,200)
+x_pos = 0
+y_pos = 0
 t.setposition(x_pos, y_pos)
 
 ### Write your code below:
-
-
-
-
+for i in range(4):
+    forward(50)
+    right(90)
 
 
 # Close window on click.
